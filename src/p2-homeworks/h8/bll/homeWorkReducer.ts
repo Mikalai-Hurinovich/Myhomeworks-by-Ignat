ca2d9900e4f7ec3c28b386de3d395fdc37da035f
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-06-19 09:55:48 +0300

    add: new 8 homework(reducer, sort)

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,20 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {
+    type: string,
+    payload: 'up' | 'down' | 18
+}
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            let newState = [...state].sort((a, b) => a.name < b.name ? -1 : 1);
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            // need to fix
-            return state
+            let newState = state.filter(el => el.age >= action.payload)
+            return newState
         }
-        default: return state
+        default:
+            return state
     }
 }
